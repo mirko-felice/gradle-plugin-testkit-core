0000378501a5c988c8144c61903dfa65f8a84ec5
Mirko Felice <mirko.felice@studio.unibo.it>
2023-05-03 12:32:35 +0200

    build: fix function call to get sonar sources

--- pluginTest/build.gradle.kts
+++ pluginTest/build.gradle.kts
@@ -60,7 +60,7 @@ fun getClassesFromSrc(project: Project): String {
 }
 
 sonarqube.properties {
-    val sources = property("sonar.sources").toString()
+    val sources = properties["sonar.sources"].toString()
     val coreSrcPath = project(":core").sourceSets.main.get().allSource.srcDirs.first { it.endsWith("kotlin") }
     property("sonar.sources", "$sources, $coreSrcPath")
 }
