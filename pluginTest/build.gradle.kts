b0288bbb055f7b9f1120ee2cfcb38a917ce9f356
Mirko Felice <mirko.felice@studio.unibo.it>
2023-05-03 14:40:19 +0200

    ci: remove coverage analysis from sonar

--- pluginTest/build.gradle.kts
+++ pluginTest/build.gradle.kts
@@ -60,12 +60,5 @@ fun getClassesFromSrc(project: Project): String {
 }
 
 sonarqube.properties {
-    val coreProjectDir = project(":core")
-    val coreSrcPath = coreProjectDir.sourceSets.main.get().allSource.srcDirs.first { it.endsWith("kotlin") }
-    val sources = properties["sonar.sources"].toString().replace("]", "").replace("[", "")
-    property("sonar.sources", "$sources,$coreSrcPath")
-
-    val coreClassesPath = getClassesFromSrc(coreProjectDir)
-    val binaries = properties["sonar.binaries"].toString().replace("]", "").replace("[", "")
-    property("sonar.binaries", "$binaries,$coreClassesPath")
+    property("sonar.coverage.jacoco.xmlReportPaths", "")
 }
