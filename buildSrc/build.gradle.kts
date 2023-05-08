94fe3cae7dc4afdd29dc9cc9a4e8c648d61bbae5
Mirko Felice <mirko.felice@studio.unibo.it>
2023-05-08 17:43:21 +0200

    build: enforce kotlin version coehrence

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.config.KotlinCompilerVersion.VERSION as KOTLIN_VERSION
+
 plugins {
     `kotlin-dsl`
 }
@@ -10,4 +12,24 @@ dependencies {
     implementation(libs.kotlin)
     implementation(libs.sonarqube)
     implementation(libs.turtle)
+}
+
+configurations.all {
+    resolutionStrategy.eachDependency {
+        if (requested.group == "org.jetbrains.kotlin" && requested.name.startsWith("kotlin")) {
+            useVersion(KOTLIN_VERSION)
+            because("All Kotlin modules should use the same version, and compiler uses $KOTLIN_VERSION")
+        }
+    }
+}
+
+kotlin {
+    target {
+        compilations.all {
+            kotlinOptions {
+                allWarningsAsErrors = true
+                freeCompilerArgs = listOf("-opt-in=kotlin.RequiresOptIn")
+            }
+        }
+    }
 }
