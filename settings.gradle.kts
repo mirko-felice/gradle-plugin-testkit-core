4616df8425ec60daf79b7450868823ded5070374
Mirko Felice <mirko.felice@studio.unibo.it>
2023-05-10 12:22:36 +0200

    chore: add gradle scan on failure + git hooks

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,26 @@
+plugins {
+    id("com.gradle.enterprise") version "3.13.1"
+    id("org.danilopianini.gradle-pre-commit-git-hooks") version "1.1.7"
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        publishOnFailure()
+    }
+}
+
+gitHooks {
+    preCommit {
+        tasks("ktlintCheck")
+    }
+    commitMsg { conventionalCommits() }
+    createHooks(true)
+}
+
 rootProject.name = "gradle-plugin-testkit"
+
 include("core")
 include("tests")
 include("gradle-plugin")
