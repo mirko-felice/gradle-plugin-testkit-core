c095cffec133a766c54bb4ebfbe98ac2db655e4e
Mirko Felice <mirko.felice@studio.unibo.it>
2023-05-12 11:08:58 +0200

    fix: publish plugin also to Maven

--- buildSrc/src/main/kotlin/library-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/library-conventions.gradle.kts
@@ -39,3 +39,52 @@ signing {
     val signingPassword: String? by project
     useInMemoryPgpKeys(signingKey, signingPassword)
 }
+
+afterEvaluate {
+    publishing {
+        repositories {
+            maven {
+                val releasesUrl = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+                val snapshotsUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+                url = uri(if (project.version.toString().endsWith("SNAPSHOT")) snapshotsUrl else releasesUrl)
+                credentials {
+                    val mavenUsername: String? by project
+                    username = mavenUsername
+                    val mavenPassword: String? by project
+                    password = mavenPassword
+                }
+            }
+        }
+        publications {
+            withType<MavenPublication>().configureEach {
+                version = project.version.toString()
+                pom {
+                    groupId = group.toString()
+                    name.set("$groupId:$artifactId")
+                    description.set(project.extra["projectDescription"].toString())
+                    url.set(githubUrl)
+                    packaging = "jar"
+                    licenses {
+                        license {
+                            name.set("MIT License")
+                            url.set("https://opensource.org/licenses/MIT")
+                        }
+                    }
+                    developers {
+                        developer {
+                            id.set("mirko-felice")
+                            name.set("Mirko Felice")
+                            email.set("mirko.felice@studio.unibo.it")
+                        }
+                    }
+                    scm {
+                        val newUrl = githubUrl.replace("https", "")
+                        connection.set("scm:git:git$newUrl.git")
+                        developerConnection.set("scm:git:ssh$newUrl.git")
+                        url.set(githubUrl)
+                    }
+                }
+            }
+        }
+    }
+}
