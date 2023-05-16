3b91bbe36c10c909381009b4aa299619cb5252e5
Mirko Felice <mirko.felice@studio.unibo.it>
2023-05-16 10:54:59 +0200

    build: add previous version to gradle.properties

--- buildSrc/src/main/kotlin/kotlin-jvm.gradle.kts
+++ buildSrc/src/main/kotlin/kotlin-jvm.gradle.kts
@@ -1,3 +1,6 @@
+import com.lordcodes.turtle.shellRun
+import java.util.*
+
 plugins {
     id("org.jetbrains.kotlin.jvm")
 }
@@ -5,4 +8,22 @@ plugins {
 repositories {
     mavenCentral()
     gradlePluginPortal()
+}
+
+val previousVersion = shellRun {
+    git.gitCommand(listOf("tag", "-l")).split("\n").takeLast(2).first()
+}
+
+val currentVersion = shellRun {
+    git.gitCommand(listOf("tag", "-l")).split("\n").last()
+}
+
+val file = file(project.rootDir.path + File.separator + "gradle.properties")
+val key = "previousProjectVersion"
+val gradleProps = Properties().also {
+    it.load(file.inputStream())
+}
+if (!gradleProps.containsKey(key) || gradleProps.getProperty(key) != previousVersion) {
+    gradleProps.setProperty(key, previousVersion)
+    gradleProps.store(file.outputStream(), null)
 }
