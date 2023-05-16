a05244cbf3ce1c73d35a74f52767bda3ebea6b6c
Mirko Felice <mirko.felice@studio.unibo.it>
2023-05-16 12:14:03 +0200

    build: fix current version

--- buildSrc/src/main/kotlin/kotlin-jvm.gradle.kts
+++ buildSrc/src/main/kotlin/kotlin-jvm.gradle.kts
@@ -10,20 +10,16 @@ repositories {
     gradlePluginPortal()
 }
 
-val previousVersion = shellRun {
-    git.gitCommand(listOf("tag", "-l")).split("\n").takeLast(2).first()
-}
-
 val currentVersion = shellRun {
     git.gitCommand(listOf("tag", "-l")).split("\n").last()
 }
 
 val file = file(project.rootDir.path + File.separator + "gradle.properties")
-val key = "previousProjectVersion"
+val key = "currentVersion"
 val gradleProps = Properties().also {
     it.load(file.inputStream())
 }
-if (!gradleProps.containsKey(key) || gradleProps.getProperty(key) != previousVersion) {
-    gradleProps.setProperty(key, previousVersion)
+if (!gradleProps.containsKey(key) || gradleProps.getProperty(key) != currentVersion) {
+    gradleProps.setProperty(key, currentVersion)
     gradleProps.store(file.outputStream(), null)
 }
