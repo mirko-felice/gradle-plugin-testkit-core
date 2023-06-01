e948e202ddc99e90c654aee0d136e9f640ae3f74
Mirko Felice <mirko.felice@studio.unibo.it>
2023-06-01 13:26:13 +0200

    test(gradle-plugin): add more tests

--- gradle-plugin/src/test/kotlin/PluginTest.kt
+++ gradle-plugin/src/test/kotlin/PluginTest.kt
@@ -21,8 +21,11 @@ class PluginTest {
     }
 
     @Test
-    fun pluginTest() {
+    fun completeConfiguration() {
         val buildContent = """
+            import io.github.mirkofelice.structure.BuildResult
+            import io.github.mirkofelice.structure.Permission
+            
             plugins {
                 id("io.github.mirko-felice.testkit")
             }
@@ -34,42 +37,115 @@ class PluginTest {
                 }
                 tests {
                     folder = file(System.getProperty("user.dir") + "/src/test/resources")
+                    println(folder)
                     test("example") {
                         configuration {
                             tasks = emptyList()
+                            println(tasks)
                             options = emptyList()
                             println(options)
+                        }
+                        expectation {
+                            result = BuildResult.SUCCESS
+                            println(result)
+                            outcomes {
+                                success = emptyList()
+                                println(success)
+                                failed = emptyList()
+                                println(failed)
+                                skipped = emptyList()
+                                println(skipped)
+                                noSource = emptyList()
+                                println(noSource)
+                                fromCache = emptyList()
+                                println(fromCache)
+                                upToDate = emptyList()
+                                println(upToDate)
+                                notExecuted = emptyList()
+                                println(notExecuted)
+                            }
+                            output {
+                                contains = emptyList()
+                                println(contains)
+                                doesntContain = emptyList()
+                                println(doesntContain)
+                            }
+                            files {
+                                existing {
+                                    file("build.gradle.kts") {
+                                        content = ""
+                                        println(content)
+                                        contentRegex = emptyList()
+                                        println(contentRegex)
+                                        permissions = listOf(Permission.R)
+                                        println(permissions)
+                                    }
+                                }
+                            }
                         }
                     }
                 }
             }
             
         """.trimIndent()
-        runGradleBuild(buildContent)
+        runGradleBuild(buildContent, false)
     }
 
     @Test
-    fun testWithJavaGradlePlugin() {
+    fun withJavaGradlePlugin() {
         val buildContent = """
             plugins {
                 `java-gradle-plugin`
                 id("io.github.mirko-felice.testkit")
             }
         """.trimIndent()
-        runGradleBuild(buildContent)
+        runGradleBuild(buildContent, false)
     }
 
-    private fun runGradleBuild(buildContent: String) {
+    @Test
+    fun wrongFolder() {
+        val buildContent = """
+            plugins {
+                id("io.github.mirko-felice.testkit")
+            }
+            
+            testkit {
+                tests {
+                    folder = file(System.getProperty("user.dir") + "/src/main")
+                }
+            }
+        """.trimIndent()
+        runGradleBuild(buildContent, true)
+    }
+
+    @Test
+    fun wrongFolderAsFile() {
+        val buildContent = """
+            plugins {
+                id("io.github.mirko-felice.testkit")
+            }
+            
+            testkit {
+                tests {
+                    folder = file(System.getProperty("user.dir") + "/src/main/kotlin/io/github/mirkofelice/plugin/TestkitPlugin.kt")
+                }
+            }
+        """.trimIndent()
+        runGradleBuild(buildContent, true)
+    }
+
+    private fun runGradleBuild(buildContent: String, failing: Boolean) {
         buildFile.writeText(buildContent)
 
         val testkitProperties = javaClass.classLoader.getResource("testkit-gradle.properties")?.readText()
         if (testkitProperties != null) {
             tempDir.resolve("gradle.properties").writeText(testkitProperties)
         }
-        GradleRunner.create()
+        val runner = GradleRunner.create()
             .withProjectDir(tempDir)
             .withPluginClasspath()
             .withArguments("testkit")
-            .build()
+
+        if (failing) runner.buildAndFail() else runner.build()
     }
 }
--- tests/src/test/resources/generateFile/wrongPermissions/build.gradle.kts
+++ tests/src/test/resources/generateFile/wrongPermissions/build.gradle.kts
@@ -1,3 +1,5 @@
+import io.github.mirkofelice.plugins.file.Permission
+
 plugins {
     id("io.github.mirko-felice.plugins.file")
 }
@@ -5,5 +7,5 @@ plugins {
 generateFile {
     fileName.set("test.txt")
     content.set("value")
-    permissions.set(listOf("R"))
+    permissions.set(listOf(Permission.R))
 }
